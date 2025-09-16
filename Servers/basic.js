b382fc60bfe0ff85ac60bcbbbbc2e4975b508439
Bruno <bruno765rez@gmail.com>
2025-09-16 11:29:49 -0300

    Criar servidor

---
+++ Servers/basic.js
@@ -0,0 +1,15 @@
+const { log } = require("console");
+const http = require("http");
+//* Create a server
+
+const server = http.createServer((req, res) => {
+  console.log(`Received ${req.method} request for: ${req.url}`);
+  res.writeHead(200, { "content-type": "text/plain" });
+  res.end("hello world from Node.JS server");
+});
+
+const PORT = 5000
+server.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+    console.log("Press Ctrl+C to stop de server");
+})
