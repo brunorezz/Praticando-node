05cfe9e4e356f5cbb4fdeb27204f98f0dfca3eef
Bruno <bruno765rez@gmail.com>
2025-09-18 10:23:04 -0300

    Adicionar comentários

--- Store-mini-project/index.js
+++ Store-mini-project/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
+
+//!Init Express
 const app = express();
 
+//! Define Routes
 app.get("/", (req, res) => res.send("Store homepage"));
 
 app.get("/products", (req, res) => res.send("Our products"));
@@ -11,4 +14,5 @@ app.get("/contact", (req, res) => res.send("Contact us"));
 
 app.post("/signup", (req, res) => res.send("Register"))
 
+//! Start Server
 app.listen(3000, () => [ console.log("Server running at http://localhost:3000") ])
