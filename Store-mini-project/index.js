6b6eecd18f9d9bdfe7ab0e0848d83e330f6dc27d
Bruno <bruno765rez@gmail.com>
2025-09-21 12:48:58 -0300

    Usando Query string

--- Store-mini-project/index.js
+++ Store-mini-project/index.js
@@ -8,11 +8,21 @@ app.get("/", (req, res) => res.send("Store homepage"));
 
 app.get("/products", (req, res) => res.send("Our products"));
 
+// app.get("/product/:id", (req, res) => console.log(req.params));
+
+// SQuery String
+app.get("/Products/search", (req, res) => {
+    console.log(req.query)
+  res.send("Query string demo");
+});
+
 app.get("/about", (req, res) => res.send("About us"));
 
 app.get("/contact", (req, res) => res.send("Contact us"));
 
-app.post("/signup", (req, res) => res.send("Register"))
+app.post("/signup", (req, res) => res.send("Register"));
 
 //! Start Server
-app.listen(3000, () => [ console.log("Server running at http://localhost:3000") ])
+app.listen(3000, () => [
+  console.log("Server running at http://localhost:3000"),
+]);
