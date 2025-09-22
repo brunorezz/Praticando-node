1233fdd45fa77690195f3d5ae616b091b4cef14f
Bruno <bruno765rez@gmail.com>
2025-09-22 11:02:23 -0300

    Lidando com erros com middleware

--- Explorando-Middlewares/app.js
+++ Explorando-Middlewares/app.js
@@ -3,8 +3,8 @@ const app = express();
 const PORT = 3000;
 //!Middleware
 app.use((req, res, next) => {
-  console.log("Middleware called");
-  next();
+  const error = new Error("Algo deu errado");
+  next(error);
 });
 
 app.get("/", (req, res) => {
@@ -14,5 +14,11 @@ app.get("/", (req, res) => {
 app.get("/About", (req, res) => {
   res.send("About us");
 });
+
+//! Error handling Middleware
+app.use((error, req, res, next) => {
+    console.log('Error', error.message)
+    res.send("Algo deu errado!")
+})
 
 app.listen(PORT, console.log("Server is up and running!"));
